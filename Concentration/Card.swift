18d1e103e6edeef12d2812a3f667189b9a62e2f9
Jason Isom <jason@Jasons-MacBook-Pro.local>
2018-01-30 16:14:44 -0700

    first commit

---
+++ Concentration/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Jason Isom on 1/29/18.
+//  Copyright © 2018 Jason Isom. All rights reserved.
+//
+
+import Foundation
---
+++ Concentration/Concentration.swift
@@ -0,0 +1,9 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Jason Isom on 1/29/18.
+//  Copyright © 2018 Jason Isom. All rights reserved.
+//
+
+import Foundation
